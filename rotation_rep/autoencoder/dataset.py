646c43a011a273a642db11089153829fa2c1c8bd
rhern <raulraulzinho@yahoo.com>
2024-03-14 18:17:45 -0400

    Extended the dataset to include metamaterials with no edges or no faces (but not both)

--- rotation_rep/autoencoder/dataset.py
+++ rotation_rep/autoencoder/dataset.py
@@ -6,13 +6,18 @@ class MetamaterialDataset(Dataset):
     # Initializes the dataset
     def __init__(self, class_size):
         super().__init__()
-        probs = [x/20 for x in range(1, 20)]
+        probs = [x/20 for x in range(20+1)]
 
         # Generates the random metamaterial data
-        per_class = class_size//(19*19)
+        per_class = class_size//(len(probs)**2)
         self.metamaterials = []
         for edge_prob in probs:
             for face_prob in probs:
+
+                # Skips empty metamaterials
+                if edge_prob == face_prob == 0.0:
+                    continue
+
                 self.metamaterials += [
                     random_metamaterial(edge_prob=edge_prob, face_prob=face_prob).sort_rep().flatten_rep()
                         for _ in range(per_class*4//5)
@@ -21,7 +26,7 @@ class MetamaterialDataset(Dataset):
                     random_metamaterial(edge_prob=edge_prob, face_prob=face_prob, grid_spacing=2).sort_rep().flatten_rep()
                         for _ in range(per_class//5)
                 ]
-            print(f"Generated metamaterials with edge probability: {edge_prob}")
+                print(f"Generated metamaterials with (edge,face) probability: ({edge_prob},{face_prob})")
         
 
     # The length of the dataset
