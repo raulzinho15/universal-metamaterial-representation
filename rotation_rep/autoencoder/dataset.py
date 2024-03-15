758f3fcdebf8ea919a45ec7a26e6fcddb9778dec
rhern <raulraulzinho@yahoo.com>
2024-03-15 11:29:26 -0400

    Changed the autoencoder structure slightly

--- rotation_rep/autoencoder/dataset.py
+++ rotation_rep/autoencoder/dataset.py
@@ -6,7 +6,7 @@ class MetamaterialDataset(Dataset):
     # Initializes the dataset
     def __init__(self, class_size):
         super().__init__()
-        probs = [x/20 for x in range(20+1)]
+        probs = [x/10 for x in range(10+1)]
 
         # Generates the random metamaterial data
         per_class = class_size//(len(probs)**2)
@@ -20,11 +20,11 @@ class MetamaterialDataset(Dataset):
 
                 self.metamaterials += [
                     random_metamaterial(edge_prob=edge_prob, face_prob=face_prob).sort_rep().flatten_rep()
-                        for _ in range(per_class//2)
+                        for _ in range(per_class*4//5)
                 ]
                 self.metamaterials += [
                     random_metamaterial(edge_prob=edge_prob, face_prob=face_prob, grid_spacing=2).sort_rep().flatten_rep()
-                        for _ in range(per_class//2)
+                        for _ in range(per_class//5)
                 ]
                 print(f"Generated metamaterials with (edge,face) probability: ({edge_prob},{face_prob})")
         
